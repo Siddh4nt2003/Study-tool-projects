5b3148f64e86b6ab63529bab4c904dbd0f069ecf
Siddhant Nema <74847742+Siddh4nt2003@users.noreply.github.com>
2022-12-16 18:55:09 +0530

    Update GaussSeidle.c

--- GaussSeidle.c
+++ GaussSeidle.c
@@ -3,18 +3,10 @@
 int main()
     {
     int a1,a2,a3,b1,b2,b3,c1,c2,c3,d1,d2,d3;
-    float prevx=0;
-    float prevy=0;
-    float prevz=0;
-    float cx=1;
-    float cy=1;
-    float cz=1;
-    float out;
+    float prevx=0,prevy=0,prevz=0,cx=1,cy=1,cz=1,out;
     int counter=0;
-
     printf("a1:");
     scanf("%i",&a1);
-
     printf("a2:");
     scanf("%i",&a2);
     printf("a3:");
@@ -55,27 +47,11 @@ int main()
             prevx = cx;
             prevy=cy;
             prevz = cz;
-            printf("%f",cx);
-            printf(" ");
-            printf("%f",cy);
-            printf("  ");
-            printf("%f",cz);
-            printf("  ");
-            printf("%i",counter);
-            printf("\n");
+            printf("%f %f %f %i\n",cx,cy,cz,counter);
         }
         
     }
-    printf("%f",cx);
-    printf("  ");
-    printf("%f",cy);
-    printf(" ");
-    printf("%f",cz);
-    printf("  ");
-    printf("%i",counter);
-    printf("\n");
-
-
+    printf("%f %f %f %i\n",cx,cy,cz,counter);
     int i;
     printf("0.Exit 1.Redo:");
     scanf("%i",&i);
